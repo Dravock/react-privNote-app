d68d442fc3cf74d86f2296c6a9eb5b8f5f9432f3
Deryan Keskin <Deryan@keskin-neubulach.de>
2023-08-09 23:19:24 +0200

    Button Texte in eine json ausgelagert für mehr dynamik

---
+++ src/includes/enums/app/ButtonTexte.js
@@ -0,0 +1,20 @@
+const Buttons = {
+        Startseite: {
+                button: {
+                    title:"Nachricht schreiben", 
+                    icons: "🎉"
+            }
+        },
+        WriteMessage:{
+            button_back: {
+                title:"Zurück zur Startseite",
+                icons: "🔙"
+            },
+            button_send: {
+                title:"Nachricht senden",
+                icons: "📝"
+            },
+        },
+    }
+
+export default Buttons
